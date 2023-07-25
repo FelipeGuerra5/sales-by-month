09257821a8f82115bed9b6f1d7185604e7cbcf8e
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-07-25 18:52:00 -0300

    NavBar Skeleton Done
    Only minimal Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Styles from './App.module.css'
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <NavBar />
+      <h1>Hello world</h1>
+    </>
   )
 }
+
+
+const NavBar = () => {
+  return (
+    <nav className={Styles.navbar} >
+      <div className={Styles.menu_user}>
+        <menu>Menu</menu>
+        <div>Username</div>
+      </div>
+      <h1 className={Styles.title}>Sales Report</h1>
+    </nav>
+  )
+}
+
 
 export default App
