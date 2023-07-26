555bea8569413c383773a7b98887a72f2970c105
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-07-25 21:40:16 -0300

    Skeleton for the page done
    1. Minimal CSS
    2. No functionality
    3. No Graph
    4. No Data

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,65 @@ import Styles from './App.module.css'
 
 function App() {
   return (
-    <>
+    <main>
       <NavBar />
-      <h1>Hello world</h1>
-    </>
+      <FilterSection />
+      <GraphView />
+    </main>
   )
 }
 
 
-const NavBar = () => {
+function NavBar() {
   return (
     <nav className={Styles.navbar} >
       <div className={Styles.menu_user}>
         <menu>Menu</menu>
-        <div>Username</div>
+        <div className={Styles.username}>
+          <div className={Styles.status}>
+          </div>
+          Username
+        </div>
       </div>
       <h1 className={Styles.title}>Sales Report</h1>
     </nav>
   )
 }
+
+function DataFilter() {
+  return (
+    <div>
+      <label for="product">Produto</label>
+      <select name="product" id="product">
+        <option value="prod1">prod1</option>
+        <option value="prod2">prod2</option>
+        <option value="prod3">prod3</option>
+      </select>
+    </div>
+
+  )
+}
+
+function FilterSection() {
+  return (
+    <section className={Styles.filter_section}>
+      <DataFilter />
+      <DataFilter />
+      <DataFilter />
+    </section>
+  )
+}
+
+function GraphView() {
+  return (
+    <div className={Styles.graph}>
+      Here will be the graph!
+    </div>
+  )
+}
+
+
+
 
 
 export default App
